4b82fbb32b36af061f031e1de0cf37261b130644
Todd A. Pratt <toddpratt978@gmail.com>
2017-07-02 14:20:34 +0000

    stub out bot file

--- bot.js
+++ bot.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const irc = require('irc');
+
+var config = require('./config.json');
+
+var client = new irc.Client(config.server, config.nick, config);
+
+/*
+ * from, to: strings, bare irc nickames
+ * text: string, the text of the message only
+ * message: object, the entire raw message object
+ */
+client.addListener('message', (from, to, text, message) => {
+  console.log('message: ', from, to, text);
+});
+
+client.addListener('raw', message => {
+  if(config.logAll || true) {
+    console.log(message.args.join(' '));
+  }
+});
