d8e9e7ec29e934cf0c3f2faa9d3f4d625637bd19
Todd A. Pratt <toddpratt978@gmail.com>
2017-07-02 15:11:34 +0000

    start web server and allow separate web and bot configs

--- app.js
+++ app.js
@@ -2,3 +2,24 @@
 
 const web = require('./web');
 const bot = require('./bot');
+
+const userConfig = require('./config.json')
+
+var webConfig = {
+    port: 3000
+};
+
+var botConfig = {
+    logAll: true
+};
+
+/* copy in user-specified configuration */
+Object.assign(webConfig, userConfig.web);
+Object.assign(botConfig, userConfig.bot);
+
+/* always control externally */
+botConfig.autoConnect = false;
+
+bot.getClient(botConfig).connect();
+web.listen(webConfig.port);
+
