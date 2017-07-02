f9c3879693254e299efe2beff5248c25e4a2f64f
Todd A. Pratt <toddpratt978@gmail.com>
2017-07-02 15:11:05 +0000

    fix scope issue

--- bot.js
+++ bot.js
@@ -2,26 +2,27 @@
 
 const irc = require('irc');
 
-/*
- * from, to: strings, bare irc nickames
- * text: string, the text of the message only
- * message: object, the entire raw message object
- */
-function messageHandler(from, to, text, message) {
-  console.log('message: ', from, to, text);
-}
+function createHandlers(client, config) {
+    /*
+     * from, to: strings, bare irc nickames
+     * text: string, the text of the message only
+     * message: object, the entire raw message object
+     */
+    client.addListener('message', (from, to, text, message) => {
+        console.log('message: ', from, to, text, message.user);
+    });
 
-function rawHandler(message) {
-  if(config.logAll) {
-    console.log(message.args.join(' '));
-  }
+    client.addListener('raw', message => {
+        if(config.logAll) {
+            console.log(message.args.join(' '));
+        }
+    });
 }
 
 var bot = module.exports = {
     getClient: config => {
         var client = new irc.Client(config.server, config.nick, config);
-        client.addListener('message', messageHandler);
-        client.addListener('raw', rawHandler);
+        createHandlers(client, config);
         return client;
     }
 };
