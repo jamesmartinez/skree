768ea5e9a65b02e54133d539e19b7fffd4664a98
Todd A. Pratt <toddpratt978@gmail.com>
2017-07-02 14:54:03 +0000

    shuffle bot module around to deal with config externally

--- bot.js
+++ bot.js
@@ -2,28 +2,27 @@
 
 const irc = require('irc');
 
-var config = {
-    logAll: true,
-    autoConnect: false  // control externally
-};
-
-/* Copy in user-specified configuration */
-Object.assign(config, require('./config.json'));
-
-var client = module.exports = new irc.Client(
-        config.server, config.nick, config);
-
 /*
  * from, to: strings, bare irc nickames
  * text: string, the text of the message only
  * message: object, the entire raw message object
  */
-client.addListener('message', (from, to, text, message) => {
+function messageHandler(from, to, text, message) {
   console.log('message: ', from, to, text);
-});
+}
 
-client.addListener('raw', message => {
+function rawHandler(message) {
   if(config.logAll) {
     console.log(message.args.join(' '));
   }
-});
+}
+
+var bot = module.exports = {
+    getClient: config => {
+        var client = new irc.Client(config.server, config.nick, config);
+        client.addListener('message', messageHandler);
+        client.addListener('raw', rawHandler);
+        return client;
+    }
+};
+
